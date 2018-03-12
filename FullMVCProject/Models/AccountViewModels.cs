dcfb1a57451a1d0041c632bad77fddf6b1df9723
IndrajSB <IndrajSB@INDRAJ>
2018-03-12 11:19:48 +0000

    finished MVCProject

--- FullMVCProject/Models/AccountViewModels.cs
+++ FullMVCProject/Models/AccountViewModels.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
+//project start
 namespace FullMVCProject.Models
 {
     public class ExternalLoginConfirmationViewModel
